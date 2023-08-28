f1e5074bb1765f904220463835efe8ead240dcb1
cha0sg0d <0xcha0sg0d@gmail.com>
2023-08-28 12:26:10 -0700

    chore: outline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Ack</h1>
+<ul>
+	<li>Spanish</li>
+	<li>Wiki</li>
+	<li>Writing</li>
+</ul>
